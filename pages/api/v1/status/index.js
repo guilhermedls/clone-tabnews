25f3bbc411a33535b9963c1bc6baf89e8d0f4bc8
Guilherme Dourado <118406726+guilhermedls@users.noreply.github.com>
2026-06-22 04:31:51 +0000

    add files  and

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,7 +1,11 @@
-function status(request, response) {
+import database from "../../../../infra/database";
+
+async function status(request, response) {
+  const result = await database.query("SELECT 1 + 1 as sum");
+  console.log(result.rows);
   response.status(200).json({
     status: 200,
-    message: "Deu boma",
+    message: "Deu bom",
   });
 }
 
